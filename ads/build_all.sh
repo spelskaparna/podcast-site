bf6dbcd95545e154f55e90737cc24d727afaadd6
Olle Landin <olle.landin@gmail.com>
2019-05-20 14:30:50 +0200

    Add: ads data

---
+++ ads/build_all.sh
@@ -0,0 +1,6 @@
+python render_ad.py  --template_path "/Users/oloflandin/Creative Cloud Files/Film/Trailers" --episode 1
+python render_ad.py  --template_path "/Users/oloflandin/Creative Cloud Files/Film/Trailers" --episode 3
+python render_ad.py  --template_path "/Users/oloflandin/Creative Cloud Files/Film/Trailers" --episode 5 --text 5a.txt --audio 5a.mp3 --output 5a
+python render_ad.py  --template_path "/Users/oloflandin/Creative Cloud Files/Film/Trailers" --episode 5 --text 5b.txt --audio 5b.mp3 --output 5b
+python render_ad.py  --template_path "/Users/oloflandin/Creative Cloud Files/Film/Trailers" --episode 21
+python render_ad.py  --template_path "/Users/oloflandin/Creative Cloud Files/Film/Trailers" --episode 65 --text 65b.txt --audio 65b.mp3 --output 65b
