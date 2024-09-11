c524995ed7df4c80d6c39d4f164fc6f838bded60
fnarkas <olle.landin@gmail.com>
2024-09-11 13:19:03 +0200

    Add: possible to upload a range of videos to be able to keep within the youtube API quota

--- shorts.py
+++ shorts.py
@@ -5,6 +5,7 @@ import re
 from episode_parser import extract_meta_data
 import youtube_test
 from datetime import datetime, timedelta
+import pandas as pd
 path = os.environ.get("TEASER_PATH")
 files = os.listdir(path)
 
@@ -13,7 +14,6 @@ files = os.listdir(path)
 class VideoFile:
     number : int
     video_path : str
-
 
 
 video_files = []
@@ -26,8 +26,10 @@ for file in files:
 # %%
 # url_lookup = youtube_test.episode_url_lookup()
 # %%
+records = []
 for i, video_file in enumerate(video_files):
-    if i != 2:
+    print(i)
+    if i not in range(3,6):
         continue
     number = video_file.number
     video_path = video_file.video_path
@@ -38,8 +40,11 @@ for i, video_file in enumerate(video_files):
     print(f"Uploading:  - {title}")
     description = "Hela avsnittet: https://pod.link/1084993748l \n\n #podcast #gaming #gamedev #gamedevelopment #gamedevlife #indiegamedev"
     tags = ["indiedev", "gamedev"]
-    start_date = datetime(2024, 9, 12)
+    start_date = datetime(2024, 9, 13)
     publish_date = (datetime.now() + timedelta(days=i)).isoformat() + 'Z'
     # related_video_url = url_lookup[number]
-
+    record = {"number":number,"title":title, "description":description, "TikTok":f"{title} \n {description}"}
+    records.append(record)
     youtube_test.upload_video(title, description, tags, publish_date, video_path)
+df = pd.DataFrame.from_records(records)
+df.to_csv("teasers.csv")
